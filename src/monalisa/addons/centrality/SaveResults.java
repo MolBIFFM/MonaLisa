adfd41b05b5ea610c4a5735c8c1409fb577f4483
Szarash <Lankov@t-online.de>
2020-03-25 17:12:28 +0100

    Refactored monalisa.addons.centrality.SaveResults to separate UI and Functionality Added a new function for export that should work. Function has not been tested yet, so old one remains for now.

--- src/monalisa/addons/centrality/SaveResults.java
+++ src/monalisa/addons/centrality/SaveResults.java
@@ -16,7 +16,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Collection;
 import javax.swing.JTable;
+import monalisa.data.pn.PetriNetFacade;
+import monalisa.data.pn.Place;
+import monalisa.data.pn.Transition;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -65,8 +69,67 @@ public class SaveResults {
             LOGGER.error("Issue during saving of calculated centrality values", ex);
         } finally {
             try {
-                writer.close();
-            } catch (IOException ex) {
+                if (writer != null) {
+                    writer.close();
+                }            } catch (IOException ex) {
+                LOGGER.error("Issue while closing writer", ex);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param file
+     * @param cc
+     * @param ecc
+     * @param bc
+     * @param ec
+     * @param pnf
+     * @throws java.io.IOException
+     */
+    public static void saveResults(File file, ClosenessCentrality cc,
+            EccentricityCentrality ecc, BetweennessCentrality bc,
+            EigenvectorCentrality ec, PetriNetFacade pnf) throws IOException {
+        LOGGER.info("Saving results of calculated centrality values");
+        Writer writer = null;
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("\"name\",\"closeness\",\"teccentricity\",\"betweenness\",\"eigenvector\"");
+        sb.append(System.getProperty("line.separator"));
+        
+        // Centralities for places, need to be adjusted for new export
+        Collection<Place> places = pnf.places();
+        for (Place p : places) {
+            sb.append(p.toString()).append(","); // Name
+            sb.append(cc.rankingPlaces.get(p.id()).toString()).append(",");
+            sb.append(ecc.rankingPlaces.get(p.id()).toString()).append(",");
+            sb.append(bc.rankingPlaces.get(p.id()).toString()).append(",");
+            sb.append(ec.rankingPlaces.get(p.id()).toString());
+            sb.append(System.getProperty("line.separator"));
+        }
+        // Removed "if(placeTable.getValueAt(row, column)!= null)" functionality, prefer controlled error over erroneous output
+
+        // Centralities for transitions, need to be adjusted for new export
+        Collection<Transition> transitions = pnf.transitions();        
+        for (Transition t : transitions) {
+            sb.append(t.toString()).append(","); // Name
+            sb.append(cc.rankingTransitions.get(t.id()).toString()).append(",");
+            sb.append(ecc.rankingTransitions.get(t.id()).toString()).append(",");
+            sb.append(bc.rankingTransitions.get(t.id()).toString()).append(",");
+            sb.append(ec.rankingTransitions.get(t.id()).toString());
+            sb.append(System.getProperty("line.separator"));
+        }
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "utf-8"));
+            writer.write(sb.toString());
+            LOGGER.info("Successfully saved calculated centrality values");
+        } catch (IOException ex) {
+            LOGGER.error("Issue during saving of calculated centrality values", ex);
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }            } catch (IOException ex) {
                 LOGGER.error("Issue while closing writer", ex);
             }
         }

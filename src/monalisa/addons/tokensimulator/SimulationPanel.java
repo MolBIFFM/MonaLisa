a1e8ad5a9b950400940e8fd5646b0b2f6557a6f3
LaraKlemt <LaraKlemt@localhost>
2023-04-05 18:39:30 +0200

    size of icons changed

--- src/monalisa/addons/tokensimulator/SimulationPanel.java
+++ src/monalisa/addons/tokensimulator/SimulationPanel.java
@@ -448,8 +448,13 @@ public class SimulationPanel extends AddonPanel implements GuiListener {
             }
         });
 
-        iconSizeSpinner.setModel(new SpinnerNumberModel(25, 5, 150, 1));
+        iconSizeSpinner.setModel(new SpinnerNumberModel(12, 5, 150, 1));
         iconSizeSpinner.setEnabled(false);
+        iconSizeSpinner.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                iconSizeSpinnerPropertyChange(evt);
+            }
+        });
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 3;
         gridBagConstraints.gridy = 0;
@@ -806,6 +811,10 @@ public class SimulationPanel extends AddonPanel implements GuiListener {
         }
     }//GEN-LAST:event_makePicActionPerformed
 
+    private void iconSizeSpinnerPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_iconSizeSpinnerPropertyChange
+        // TODO add your handling code here:
+    }//GEN-LAST:event_iconSizeSpinnerPropertyChange
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel arrowSizeLabel;
     protected javax.swing.JSpinner arrowSizeSpinner;
@@ -923,6 +932,7 @@ public class SimulationPanel extends AddonPanel implements GuiListener {
         arrowSizeSpinner.setValue(netViewer.getArrowSize());
         edgeSizeSpinner.setValue(netViewer.getEdgeSize());
         iconSizeSpinner.setValue(netViewer.getIconSize());
+        LOGGER.info("newviewer " + netViewer.getIconSize()+ ""); // TODO delete
         //enables/disables GUI-components
         startSimulationJButton.setEnabled(false);
         endSimulationJButton.setEnabled(true);
@@ -959,8 +969,16 @@ public class SimulationPanel extends AddonPanel implements GuiListener {
         simulationMan.getPreferences().put("EnablePlotting", true);
         Map<Place, Boolean> placesToPlot = new HashMap<>();
         simulationMan.getPreferences().put("PlacesToPlot", placesToPlot);
+        
+        //assign correct values for the spinner // TODO delete
+        //fontSizeSpinner.setValue(netViewer.getFontSize());// TODO delete
+        //arrowSizeSpinner.setValue(netViewer.getArrowSize());// TODO delete
+        //edgeSizeSpinner.setValue(netViewer.getEdgeSize());// TODO delete
+        iconSizeSpinner.setValue(netViewer.getIconSize());// TODO delete? size doesnt change anymore
+        
         //get the size of vertices in NetViewer
         vertexSize = (int) iconSizeSpinner.getValue();
+        LOGGER.info("vertexSize "+vertexSize+""); // TODO delete
         //create clear history
         simulationMan.lastHistoryStep = -1;
         simulationMan.historyArrayList = new ArrayList<>();

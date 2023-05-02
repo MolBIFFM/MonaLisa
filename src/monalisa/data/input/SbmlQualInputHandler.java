ee574cbbe9f4668604a08f46b8f38d918c64c2c1
annkathrin <annkathrin@localhost>
2023-05-02 12:16:06 +0200

    Layout added in the conversion.

--- src/monalisa/data/input/SbmlQualInputHandler.java
+++ src/monalisa/data/input/SbmlQualInputHandler.java
@@ -20,11 +20,16 @@ import org.sbml.jsbml.ext.qual.Transition;
 import org.sbml.jsbml.ext.qual.Input;
 import org.sbml.jsbml.SpeciesReference;
 import org.sbml.jsbml.ext.qual.Output;
+import org.sbml.jsbml.ext.layout.Layout;
+import org.sbml.jsbml.ext.layout.LayoutModelPlugin;
+import org.sbml.jsbml.ext.layout.LayoutConstants;
 import org.sbml.jsbml.ext.qual.QualitativeSpecies;
+import org.sbml.jsbml.ListOf;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+
 
 
 /**
@@ -42,105 +47,115 @@ public class SbmlQualInputHandler {
         // get the QualModelPlugin object
         QualModelPlugin qual_ext = (QualModelPlugin) model.getPlugin("qual");
 
-
-        List<String> qual_transitions_list = new ArrayList<>();
-        List<List <Integer>> qual_input_level_list = new ArrayList<>();
-        List<List <String>> qual_input_species_list = new ArrayList<>();
-        List<List <Integer>> qual_output_level_list = new ArrayList<>();
-        List<List <String>> qual_output_species_list = new ArrayList<>();
-        List<List <Input>> qual_inputs_list = new ArrayList<>();
-        List<List <Output>> qual_outputs_list = new ArrayList<>();
-        List<Input> qual_input = null;
+        
+        // initialize variables
+        List<String> qualTransitionsList = new ArrayList<>();
+        List<List <Integer>> qualInputLevelList = new ArrayList<>();
+        List<List <String>> qualInputSpeciesList = new ArrayList<>();
+        List<List <Integer>> qualOutputLevelList = new ArrayList<>();
+        List<List <String>> qualOutputSpeciesList = new ArrayList<>();
+        List<List <Input>> qualInputsList = new ArrayList<>();
+        List<List <Output>> qualOutputsList = new ArrayList<>();
+        List<Input> qualInput = null;
 
 
         // get the list of qualitative species and transitions
-        List<QualitativeSpecies> qual_species_list = qual_ext.getListOfQualitativeSpecies();
-        List<Transition> qual_transitions_list_one = qual_ext.getListOfTransitions();
+        List<QualitativeSpecies> qualSpeciesList = qual_ext.getListOfQualitativeSpecies();
+        List<Transition> qualTransitionsListOne = qual_ext.getListOfTransitions();
 
 
         // get the list of transition ID' as strings
-        for (int i = 0; i < qual_transitions_list_one.size(); i++) {
-            Transition t = qual_transitions_list_one.get(i);
-            String transition_id = t.getId();
-            qual_transitions_list.add(transition_id);
+        for (int i = 0; i < qualTransitionsListOne.size(); i++) {
+            Transition t = qualTransitionsListOne.get(i);
+            String transitionId = t.getId();
+            qualTransitionsList.add(transitionId);
         }
 
 
         // get the input species and input edge weight of the transitions
-        for (int i = 0; i < qual_transitions_list.size(); i++) {
-            List<Integer> input_level = new ArrayList<>();
-            List<String> input_species = new ArrayList<>();
-            Transition qual_transition = qual_ext.getTransition(i);
-            qual_input = qual_transition.getListOfInputs();
-            qual_inputs_list.add(qual_input);
+        for (int i = 0; i < qualTransitionsList.size(); i++) {
+            List<Integer> inputLevel = new ArrayList<>();
+            List<String> inputSpecies = new ArrayList<>();
+            Transition qualTransition = qual_ext.getTransition(i);
+            qualInput = qualTransition.getListOfInputs();
+            qualInputsList.add(qualInput);
 
-            for (int j = 0; j < qual_input.size(); j++) {
-                Integer qual_input_level = null;
-                if (qual_input.get(j).isSetThresholdLevel() == true) {
-                    qual_input_level = qual_input.get(j).getThresholdLevel();
+            for (int j = 0; j < qualInput.size(); j++) {
+                Integer qualInputLevel = null;
+                if (qualInput.get(j).isSetThresholdLevel() == true) {
+                    qualInputLevel = qualInput.get(j).getThresholdLevel();
                 }
                 else {
-                    qual_input_level = 1;
+                    qualInputLevel = 1;
                 }
 
-                String qual_input_species = qual_input.get(j).getQualitativeSpecies();
-                input_level.add(qual_input_level);
-                input_species.add(qual_input_species);
+                String qualInputSpecies = qualInput.get(j).getQualitativeSpecies();
+                inputLevel.add(qualInputLevel);
+                inputSpecies.add(qualInputSpecies);
             }
-            qual_input_level_list.add(input_level);
-            qual_input_species_list.add(input_species);
+            qualInputLevelList.add(inputLevel);
+            qualInputSpeciesList.add(inputSpecies);
         }
 
 
         // get the output species and output edge weight of the transitions
-        for (int i = 0; i < qual_transitions_list.size(); i++) {
-            List<Integer> output_level = new ArrayList<>();
-            List<String> output_species = new ArrayList<>();
-            Transition qual_transition = qual_ext.getTransition(i);
-            List<Output> qual_output = qual_transition.getListOfOutputs();
-            qual_outputs_list.add(qual_output);
+        for (int i = 0; i < qualTransitionsList.size(); i++) {
+            List<Integer> outputLevel = new ArrayList<>();
+            List<String> outputSpecies = new ArrayList<>();
+            Transition qualTransition = qual_ext.getTransition(i);
+            List<Output> qualOutput = qualTransition.getListOfOutputs();
+            qualOutputsList.add(qualOutput);
 
-            for (int j = 0; j < qual_output.size(); j++) {
-                Integer qual_output_level = null;
-                if (qual_output.get(j).isSetOutputLevel() == true) {
-                    qual_output_level = qual_output.get(j).getOutputLevel();
+            for (int j = 0; j < qualOutput.size(); j++) {
+                Integer qualOutputLevel = null;
+                if (qualOutput.get(j).isSetOutputLevel() == true) {
+                    qualOutputLevel = qualOutput.get(j).getOutputLevel();
                 }
                 else {
-                    qual_output_level = 1;
+                    qualOutputLevel = 1;
                 }
 
-                output_level.add(qual_output_level);
-                String qual_output_species = qual_output.get(j).getQualitativeSpecies();
-                output_species.add(qual_output_species);
+                outputLevel.add(qualOutputLevel);
+                String qualOutputSpecies = qualOutput.get(j).getQualitativeSpecies();
+                outputSpecies.add(qualOutputSpecies);
             }
 
-            qual_output_level_list.add(output_level);
-            qual_output_species_list.add(output_species);
+            qualOutputLevelList.add(outputLevel);
+            qualOutputSpeciesList.add(outputSpecies);
         }
 
 
         // get the initial token amount
         List<Integer> initial_level = new ArrayList<>();
-        for (QualitativeSpecies species : qual_species_list) {
-            Integer qual_initial_level = null;
+        for (QualitativeSpecies species : qualSpeciesList) {
+            Integer qualInitialLevel = null;
             if (species.isSetInitialLevel() == true) {
-                qual_initial_level = species.getInitialLevel();
+                qualInitialLevel = species.getInitialLevel();
             }
             else {
-                qual_initial_level = 0;
+                qualInitialLevel = 0;
             }
-            initial_level.add(qual_initial_level);
+            initial_level.add(qualInitialLevel);
         }
-
-
+        
+      
         // create a new sbml document to put in the sbmlqual document information
-        SBMLDocument converted_doc = new SBMLDocument(document.getLevel(), document.getVersion());
-        Model converted_model = converted_doc.createModel("petri_net");
-
+        SBMLDocument convertedDoc = new SBMLDocument(document.getLevel(), document.getVersion());
+        Model convertedModel = convertedDoc.createModel("petri_net");
+        
+        
+        // get the list of layouts from the qual file and add it to the new file
+        LayoutModelPlugin plugin = (LayoutModelPlugin) document.getModel().getPlugin(LayoutConstants.namespaceURI);
+        ListOf<Layout> layoutList = plugin.getListOfLayouts();
+        if (!layoutList.isEmpty()) {
+            LayoutModelPlugin newplugin = (LayoutModelPlugin) convertedDoc.getModel().getPlugin(LayoutConstants.namespaceURI);
+            newplugin.setListOfLayouts(layoutList);
+        }
+        
 
         // set the model ID
         if (model.isSetId() == true) {
-            converted_model.setId(model.getId());
+            convertedModel.setId(model.getId());
         }
 
 
@@ -150,16 +165,16 @@ public class SbmlQualInputHandler {
             String prefix = entry.getKey();
             String uri = entry.getValue();
             if (!prefix.contains("qual")) {
-                converted_doc.addDeclaredNamespace(prefix, uri);
+                convertedDoc.addDeclaredNamespace(prefix, uri);
             }
         }
 
         
         // set the qualitative species as species
-        List<QualitativeSpecies> places = qual_species_list;
+        List<QualitativeSpecies> places = qualSpeciesList;
         int index = 0;
         for (QualitativeSpecies p : places) {
-            Species s = converted_model.createSpecies();
+            Species s = convertedModel.createSpecies();
             s.setId(p.getId());
             s.setCompartment(p.getCompartment());
 
@@ -187,35 +202,35 @@ public class SbmlQualInputHandler {
 
 
         // set the transitions as reactions with reactants and products
-        for (int t = 0; t < qual_transitions_list.size(); t++) {
-            Reaction r = converted_model.createReaction();
-            r.setId(qual_transitions_list.get(t));
+        for (int t = 0; t < qualTransitionsList.size(); t++) {
+            Reaction r = convertedModel.createReaction();
+            r.setId(qualTransitionsList.get(t));
             r.setReversible(false);
 
-            if (!qual_transitions_list_one.get(t).getCVTerms().isEmpty()) {
-                for (int cv = 0; cv < qual_transitions_list_one.get(t).getCVTerms().size(); cv++) {
-                    r.addCVTerm(qual_transitions_list_one.get(t).getCVTerms().get(cv));
+            if (!qualTransitionsListOne.get(t).getCVTerms().isEmpty()) {
+                for (int cv = 0; cv < qualTransitionsListOne.get(t).getCVTerms().size(); cv++) {
+                    r.addCVTerm(qualTransitionsListOne.get(t).getCVTerms().get(cv));
                 }
             }
 
-            if (!qual_transitions_list_one.get(t).getSBOTermID().isEmpty()) {
-                r.setSBOTerm(qual_transitions_list_one.get(t).getSBOTermID());
+            if (!qualTransitionsListOne.get(t).getSBOTermID().isEmpty()) {
+                r.setSBOTerm(qualTransitionsListOne.get(t).getSBOTermID());
             }
 
-            for (int p = 0; p < qual_input_species_list.get(t).size(); p++) {
+            for (int p = 0; p < qualInputSpeciesList.get(t).size(); p++) {
                 SpeciesReference s = r.createReactant();
-                s.setSpecies(qual_input_species_list.get(t).get(p));
-                s.setStoichiometry(qual_input_level_list.get(t).get(p));
+                s.setSpecies(qualInputSpeciesList.get(t).get(p));
+                s.setStoichiometry(qualInputLevelList.get(t).get(p));
             }
 
-            for (int p = 0; p < qual_output_species_list.get(t).size(); p++) {
+            for (int p = 0; p < qualOutputSpeciesList.get(t).size(); p++) {
                 SpeciesReference s = r.createProduct();
-                s.setSpecies(qual_output_species_list.get(t).get(p));
-                s.setStoichiometry(qual_output_level_list.get(t).get(p));
+                s.setSpecies(qualOutputSpeciesList.get(t).get(p));
+                s.setStoichiometry(qualOutputLevelList.get(t).get(p));
             }
         }
 
-        return converted_doc;
+        return convertedDoc;
 
     }
 }

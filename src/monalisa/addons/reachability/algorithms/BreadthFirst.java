737014f4d1b3874286e3cca8d137cd73d747f693
Midazolami <khaas91@gmx.de>
2024-08-08 17:39:51 +0200

    Fucked aup, started new. -> Reachability nodes singular added to a list. How to store first node? -> Prev[y/n]

--- src/monalisa/addons/reachability/algorithms/BreadthFirst.java
+++ src/monalisa/addons/reachability/algorithms/BreadthFirst.java
@@ -5,11 +5,11 @@
  */
 package monalisa.addons.reachability.algorithms;
 
-import com.sun.source.tree.Tree;
-import edu.uci.ics.jung.visualization.util.LabelWrapper;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import monalisa.addons.reachability.Pathfinder;
 import monalisa.addons.reachability.ReachabilityEdge;
 import monalisa.addons.reachability.ReachabilityEvent;
@@ -27,27 +27,24 @@ import org.apache.logging.log4j.Logger;
 public class BreadthFirst extends AbstractReachabilityAlgorithm {
 
     private static final Logger LOGGER = LogManager.getLogger(BreadthFirst.class);
-    private HashMap<Place, Long> eStart;
-    private HashMap<Place, Long> eTarget;
 
     public BreadthFirst(Pathfinder pf, HashMap<Place, Long> marking, HashMap<Place, Long> target) {
         super(pf, marking, target);
+        
     }
 
+   
     /**
-     * @author Kristin Haas
+     * 
      * @param pf
-     * @param start
-     * @param target 
-     *
-    public BreadthFirst(Pathfinder pf, ReachabilityNode start,  ReachabilityNode target) {
-        super(pf, start, target);
-    }*/
-    
-    public BreadthFirst(Pathfinder pf, HashMap<Place, Long> marking,HashMap<Place, Long> target, HashMap<Place, Long> eStart, HashMap<Place, Long> eTaget ){
+     * @param marking
+     * @param target
+     * @param eStart
+     * @param eTaget 
+     */
+    public BreadthFirst(Pathfinder pf, HashMap<Place, Long> marking,HashMap<Place, Long> target, HashMap<Place, Long> eStart, HashMap<Place, Long> eTaget ) throws InterruptedException{
         super(pf, marking, target);
-        this.eStart = eStart;
-        this.eTarget = eTaget;
+        runExplicit();
         
     }
     
@@ -55,147 +52,111 @@ public class BreadthFirst extends AbstractReachabilityAlgorithm {
      * include query  to ask for specific source and tagret
      */
     @Override
-    public void run() {
+   public void run() {
         LOGGER.debug("Starting Breadth First Algorithm.");
         fireReachabilityUpdate(ReachabilityEvent.Status.STARTED, 0, null);
         int counter = 0;
         HashSet<ReachabilityNode> vertices = new HashSet<>();
         HashSet<ReachabilityEdge> edges = new HashSet<>();
         // initialize for m0 as root
-        // If explicit start and target exist
-        
-        if(eStart != null && eTarget != null){
-            if(eStart.keySet().iterator().next().toString() == eTarget.keySet().iterator().next().toString()){
-                //System.out.println("TEST "+eStar+" "+eTar);
-                fireReachabilityUpdate(ReachabilityEvent.Status.SUCCESS, counter, null);
-                return;
-                
-    
+        ReachabilityNode root = new ReachabilityNode(marking, null);
+        tar = new ReachabilityNode(target, null);
+        vertices.add(root);
+        ArrayList<ReachabilityNode> workingList = new ArrayList<>();
+        workingList.add(root);
+        while (!workingList.isEmpty() && !isInterrupted()) {
+            LOGGER.debug("Starting expansion for a new node."); // debug
+            counter += 1;
+            if (counter % 100 == 0) {
+                fireReachabilityUpdate(ReachabilityEvent.Status.PROGRESS, counter, null);
             }
-            //Somewhere here seems to be the problem
-            ReachabilityNode root = new ReachabilityNode(marking, null);
-            System.out.println("REACHABILITYNODE: "+root.getMarking());
-           // ReachabilityNode root = new ReachabilityNode(start, null);
-            tar = new ReachabilityNode(target, null);
-            vertices.add(root);
-            ArrayList<ReachabilityNode> workingList = new ArrayList<>();
-            workingList.add(root);
-            while (!workingList.isEmpty() && !isInterrupted()) {
-                LOGGER.debug("Starting expansion for a new node."); // debug
-                counter += 1;
-                if (counter % 100 == 0) {
-                    fireReachabilityUpdate(ReachabilityEvent.Status.PROGRESS, counter, null);
+            ReachabilityNode workingNode = workingList.get(0);
+            LOGGER.debug("Current marking:" + workingNode.getMarking().toString());
+            workingList.remove(0);
+            HashSet<Transition> activeTransitions = pf.computeActive(workingNode.getMarking());
+            for (Transition t : activeTransitions) {
+                LOGGER.debug("Created new node by firing transition " + t.getProperty("name") + ".");  // debug
+                HashMap<Place, Long> mNew = pf.computeMarking(workingNode.getMarking(), t);
+                ReachabilityNode newNode = new ReachabilityNode(mNew, workingNode);
+                // Algorithm terminates on finding m*
+                if (newNode.equals(tar)) {
+                    tar = newNode;
+                    vertices.add(tar);
+                    edges.add(new ReachabilityEdge(workingNode, tar, t));
+                    g = new ReachabilityGraph(vertices, edges);
+                    fireReachabilityUpdate(ReachabilityEvent.Status.SUCCESS, counter, backtrack());
+                    LOGGER.debug("Target marking has been reached.");
+                    return;
                 }
-                ReachabilityNode workingNode = workingList.get(0);
-                System.out.println("COUNTER: "+counter+" WorkingList: "+workingList.get(0).getMarking()+" 0 "+pf.computeActive(workingNode.getMarking()));
-                LOGGER.debug("Current marking:" + workingNode.getMarking().toString());
-                workingList.remove(0);
-                HashSet<Transition> activeTransitions = pf.computeActive(workingNode.getMarking());
-                for (Transition t : activeTransitions) {
-                    if(workingNode.getMarking()!= null && t != null){
-                        LOGGER.debug("Created new node by firing transition " + t.getProperty("name") + ".");  // debug
-                        HashMap<Place, Long> mNew = pf.computeMarking(workingNode.getMarking(), t);
-                        ReachabilityNode newNode = new ReachabilityNode(mNew, workingNode);
-                        // Algorithm terminates on finding m*
-                        if (newNode.equals(tar)) {
-                            tar = newNode;
-                            vertices.add(tar);
-                            edges.add(new ReachabilityEdge(workingNode, tar, t));
-                            g = new ReachabilityGraph(vertices, edges);
-                            fireReachabilityUpdate(ReachabilityEvent.Status.SUCCESS, counter, backtrack());
-                            LOGGER.debug("Target marking has been reached.");
-                            return;
-                        }
-                        boolean unvisited = true;
-                        for (ReachabilityNode v : vertices) {
-                            if (v.equals(newNode)) {
-                                unvisited = false;
-                                edges.add(new ReachabilityEdge(workingNode, v, t));
-                                break;
-                            }
-                        }
-                        if (unvisited) {
-                            vertices.add(newNode);
-                            workingList.add(newNode);
-                            edges.add(new ReachabilityEdge(workingNode, newNode, t));
-                        }
+                boolean unvisited = true;
+                for (ReachabilityNode v : vertices) {
+                    if (v.equals(newNode)) {
+                        unvisited = false;
+                        edges.add(new ReachabilityEdge(workingNode, v, t));
+                        break;
                     }
                 }
-
+                if (unvisited) {
+                    vertices.add(newNode);
+                    workingList.add(newNode);
+                    edges.add(new ReachabilityEdge(workingNode, newNode, t));
+                }
             }
-            if (isInterrupted()) {
-                LOGGER.warn("Execution has been aborted.");
-                fireReachabilityUpdate(ReachabilityEvent.Status.ABORTED, counter, null);
-            } else {
-                LOGGER.info("Target marking could not be reached from start marking.");
-                g = new ReachabilityGraph(vertices, edges);
-                fireReachabilityUpdate(ReachabilityEvent.Status.FAILURE, counter, null);
-            }
-         
-            
         }
-        else{
-
-            ReachabilityNode root = new ReachabilityNode(marking, null);
-            tar = new ReachabilityNode(target, null);
-            vertices.add(root);
-            ArrayList<ReachabilityNode> workingList = new ArrayList<>();
-            workingList.add(root);
-            while (!workingList.isEmpty() && !isInterrupted()) {
-                LOGGER.debug("Starting expansion for a new node."); // debug
-                counter += 1;
-                if (counter % 100 == 0) {
-                    fireReachabilityUpdate(ReachabilityEvent.Status.PROGRESS, counter, null);
-                }
-                ReachabilityNode workingNode = workingList.get(0);
-
-                LOGGER.debug("Current marking:" + workingNode.getMarking().toString());
-                workingList.remove(0);
-                HashSet<Transition> activeTransitions = pf.computeActive(workingNode.getMarking());
-                for (Transition t : activeTransitions) {
-                    LOGGER.debug("Created new node by firing transition " + t.getProperty("name") + ".");  // debug
-                    HashMap<Place, Long> mNew = pf.computeMarking(workingNode.getMarking(), t);
-                    ReachabilityNode newNode = new ReachabilityNode(mNew, workingNode);
-                    // Algorithm terminates on finding m*
-                    if (newNode.equals(tar)) {
-                        tar = newNode;
-                        vertices.add(tar);
-                        edges.add(new ReachabilityEdge(workingNode, tar, t));
-                        g = new ReachabilityGraph(vertices, edges);
-                        fireReachabilityUpdate(ReachabilityEvent.Status.SUCCESS, counter, backtrack());
-                        LOGGER.debug("Target marking has been reached.");
-                        return;
-                    }
-                    boolean unvisited = true;
-                    for (ReachabilityNode v : vertices) {
-                        if (v.equals(newNode)) {
-                            unvisited = false;
-                            edges.add(new ReachabilityEdge(workingNode, v, t));
-                            break;
-                        }
-                    }
-                    if (unvisited) {
-                        vertices.add(newNode);
-                        workingList.add(newNode);
-                        edges.add(new ReachabilityEdge(workingNode, newNode, t));
-                    }
-                }
-
-            }
-            if (isInterrupted()) {
-                LOGGER.warn("Execution has been aborted.");
-                fireReachabilityUpdate(ReachabilityEvent.Status.ABORTED, counter, null);
-            } else {
-                LOGGER.info("Target marking could not be reached from start marking.");
-                g = new ReachabilityGraph(vertices, edges);
-                fireReachabilityUpdate(ReachabilityEvent.Status.FAILURE, counter, null);
-            }
-            }
-         
+        if (isInterrupted()) {
+            LOGGER.warn("Execution has been aborted.");
+            fireReachabilityUpdate(ReachabilityEvent.Status.ABORTED, counter, null);
+        } else {
+            LOGGER.info("Target marking could not be reached from start marking.");
+            g = new ReachabilityGraph(vertices, edges);
+            fireReachabilityUpdate(ReachabilityEvent.Status.FAILURE, counter, null);
+        }
     }
+
 
     @Override
     public void computePriority(ReachabilityNode node) {
         // Breadth First Search does not use a priority.
+    }
+
+    @Override
+    public void runExplicit() throws InterruptedException {
+        LOGGER.debug("Starting BFS with specific start and target node");
+        fireReachabilityUpdate(ReachabilityEvent.Status.STARTED, 0, null);
+        int counter = 0;
+        HashSet<ReachabilityNode> vertices = new HashSet<>();
+        HashSet<ReachabilityEdge> edge = new HashSet<>();
+        
+        
+        ArrayList<ReachabilityNode> rNodeList = new ArrayList<>();
+        System.out.println("MARkingSize; "+marking.size());
+        ReachabilityNode checkNode = new ReachabilityNode(null, null);
+        for(Map.Entry<Place, Long> entry: marking.entrySet()){
+            System.out.println("Erstellen: "+ entry);
+            HashMap<Place, Long> work = new HashMap<>();
+            
+            if(rNodeList.isEmpty()){
+                work.put(entry.getKey(), entry.getValue());
+                ReachabilityNode firstNode = new ReachabilityNode(work, null);
+                rNodeList.add(firstNode);
+                checkNode = firstNode;
+                
+            }
+            ReachabilityNode lastNode = rNodeList.getLast();
+            work.put(entry.getKey(), entry.getValue());
+            ReachabilityNode currNode = new ReachabilityNode(work, lastNode);
+            rNodeList.add(currNode);
+              
+        }
+        rNodeList.remove(1);
+        for(int i = 0; i < rNodeList.size(); i++){
+            try {
+                System.out.println("LISTE: "+ rNodeList.get(i).getMarking()+" Prev: "+rNodeList.get(i).getPrev().getMarking());
+                
+            } catch (NullPointerException e) {
+                System.out.println("LISTE: "+ rNodeList.get(i).getMarking()+ " Prev: none");
+            }
+        }
+        
     }
 }
